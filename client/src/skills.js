672c0e1d0dfe0ea02710e9da9f05f56dc7026977
NithiNWeb4 <lpun490@gmail.com>
2024-08-11 12:58:39 +0530

    skills added

--- client/src/skills.js
+++ client/src/skills.js
@@ -10,9 +10,11 @@ function Skills() {
         { name: "MERN Stack", details: "MongoDB, Express.js, React.js, Node.js" },
         { name: "Bootstrap", details: "Responsive design and UI components" },
         { name: "React.js", details: "Building dynamic user interfaces" },
+        { name: "Express js", details: "Backend Frame work for server maintanance" },
         { name: "MongoDB", details: "NoSQL database for modern applications" },
         { name: "HTML", details: "Markup language for web development" },
-        { name: "CSS", details: "Styling web pages with modern techniques" }
+        { name: "CSS", details: "Styling web pages with modern techniques" },
+        {name:"web development(without react)",details:"i can develop website without react js"}
       ]
     },
     {
@@ -20,21 +22,29 @@ function Skills() {
       skills: [
         { name: "Git", details: "Version control system" },
         { name: "GitHub", details: "Hosting for version control and collaboration" },
-        { name: "Truffle", details: "Development framework for Ethereum" },
-        { name: "Solidity", details: "Programming language for Ethereum smart contracts" },
+        { name: "Truffle", details: "Testing framework for Ethereum" },
+        { name: "Foundry", details: "Testing framework for Ethereum" },
+        { name: "Postman", details: "Backend Testing Framework for express js server" },
+        
         { name: "AWS Services", details: "Connecting websites to cloud services" },
-        { name: "IPFS", details: "Decentralized storage solution" }
+
+        { name: "IPFS", details: "Decentralized storage solution" },
+
       ]
     },
     {
-      category: "Blockchain",
+      category: " Ethereum Blockchain ",
       skills: [
         { name: "Ethereum Blockchain", details: "Building decentralized applications (dApps)" },
         { name: "Solidity", details: "Developing and deploying smart contracts" },
+        { name: "Foundry", details: "Testing and deploying smart contracts" },
         { name: "Truffle", details: "Testing and deploying smart contracts" },
+        
+        { name: "Security", details: " Auditing solidity smart contracts" },
         { name: "Solidity Attacks Auditing", details: "Identifying and mitigating security vulnerabilities" },
         {name:"Defi",details:"i can develop Defi applications!"},
-        {name:"Web3.js",details:"i can develop Dapps with Web3 library in javascript@!"}
+        {name:"Web3.js",details:"i can develop Dapps with Web3 library in javascript@!"},
+        { name: "MetaMask", details: "Wallet For Decentralized Applications" },
     
     
     ]
@@ -43,7 +53,34 @@ function Skills() {
       category: "Other",
       skills: [
         { name: "Data Structures", details: "Understanding of various data structures" },
-        { name: "React.js Redux", details: "State management for React applications" }
+        { name: "React.js Redux", details: "State management for React applications" },
+      
+      ]
+    }
+    ,
+    {
+
+      category:"Programming Languages",
+      skills:[
+        {name:"Python" ,details:"used for Dsa and problem solving"},
+        {name:"Javascipt",details:"for Web development"},
+        {name:"Nodejs-express",details:"for backend server"},
+        {name:"solidity",details:"Writing Smart Contracts"},
+
+
+      ]
+
+    }
+
+    ,
+    {category:"Services",
+      skills:[
+        {name:"Render",details:"For Web Hosting !"},
+        {name:"Cloudinary",details:"Medai storage"},
+        {name:"SendGrid",details:"mails sending!"},
+        {name:"RazorPay",details:"For Payment"},
+        {name:"Mongodb",details:"For data storage"},
+        {name:"pinata-Ipfs",details:"Decentralized storage for data"}
       ]
     }
   ];
